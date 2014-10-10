ec8a59bebbbc9f3e10875482184fdb7a7eb66d37
TAKAHASHI Yuki <y-takahashi@begood-tech.com>
2014-10-10 19:23:16 +0900

    first commit

---
+++ appinfo/app.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * ownCloud - Files Softlayer
+ *
+ * @copyright 2014 Begood Technology Corp. <owncloud@begood-tech.com>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+$l = \OC_L10N::get('files_external');
+
+OC::$CLASSPATH['OC\Files\Storage\Softlayer'] = 'files_softlayer/lib/softlayer.php';
+
+OC_Mount_Config::registerBackend('\OC\Files\Storage\Softlayer', array(
+	'backend' => (string)$l->t('Softlayer Object Storage'),
+	'priority' => 100,
+	'configuration' => array(
+		'user' => (string)$l->t('Username (required)'),
+		'root' => (string)$l->t('Bucket (required)'),
+		'token' => '*'.$l->t('Password (required for OpenStack Object Storage)'),
+		'host' => '&'.$l->t('URL of identity endpoint (required for OpenStack Object Storage)'),
+	),
+	'has_dependencies' => true));
